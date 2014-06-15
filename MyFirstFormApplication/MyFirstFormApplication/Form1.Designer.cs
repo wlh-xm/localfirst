588bf6aef82a6b5eb8b45d42c7e4c3e4de507453
wlh-xm <dearming@foxmail.com>
2014-06-15 12:47:07 +0800

    3

---
+++ MyFirstFormApplication/MyFirstFormApplication/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace MyFirstFormApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.导航ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.主页ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.前进ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.返回ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gobutton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.导航ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(624, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 导航ToolStripMenuItem
+            // 
+            this.导航ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.主页ToolStripMenuItem,
+            this.前进ToolStripMenuItem,
+            this.返回ToolStripMenuItem});
+            this.导航ToolStripMenuItem.Name = "导航ToolStripMenuItem";
+            this.导航ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.导航ToolStripMenuItem.Text = "导航";
+            // 
+            // 主页ToolStripMenuItem
+            // 
+            this.主页ToolStripMenuItem.Name = "主页ToolStripMenuItem";
+            this.主页ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.主页ToolStripMenuItem.Text = "主页";
+            this.主页ToolStripMenuItem.Click += new System.EventHandler(this.主页ToolStripMenuItem_Click);
+            // 
+            // 前进ToolStripMenuItem
+            // 
+            this.前进ToolStripMenuItem.Name = "前进ToolStripMenuItem";
+            this.前进ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.前进ToolStripMenuItem.Text = "返回";
+            this.前进ToolStripMenuItem.Click += new System.EventHandler(this.前进ToolStripMenuItem_Click);
+            // 
+            // 返回ToolStripMenuItem
+            // 
+            this.返回ToolStripMenuItem.Name = "返回ToolStripMenuItem";
+            this.返回ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.返回ToolStripMenuItem.Text = "前进";
+            this.返回ToolStripMenuItem.Click += new System.EventHandler(this.返回ToolStripMenuItem_Click);
+            // 
+            // gobutton
+            // 
+            this.gobutton.Location = new System.Drawing.Point(361, 29);
+            this.gobutton.Name = "gobutton";
+            this.gobutton.Size = new System.Drawing.Size(75, 23);
+            this.gobutton.TabIndex = 1;
+            this.gobutton.Text = "转到";
+            this.gobutton.UseVisualStyleBackColor = true;
+            this.gobutton.Click += new System.EventHandler(this.gobutton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "http://www.baidu.com",
+            "http://www.weibo.com"});
+            this.comboBox1.Location = new System.Drawing.Point(25, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(330, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(25, 66);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(587, 299);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 387);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.gobutton);
+            this.Controls.Add(this.webBrowser1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Web浏览器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 导航ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 主页ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 前进ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 返回ToolStripMenuItem;
+        private System.Windows.Forms.Button gobutton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
